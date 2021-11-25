359ada4b1a38ea338a15f1c6a59bbe64e2e1ef01
Sim Wooi Zhi <wooizhi.sim@affinhwangam.com>
2021-11-25 11:11:20 +0800

    fixes the task 670 672 674

--- AnimalAdoption.Common.Logic/AnimalService.cs
+++ AnimalAdoption.Common.Logic/AnimalService.cs
@@ -7,9 +7,9 @@ namespace AnimalAdoption.Common.Logic
     public class AnimalService
     {
         public Animal[] ListAnimals => new Animal[] {
-            new Animal { Id = 1, Name = "Sedi", Age = 50, Description = "Soft natured" },
-            new Animal { Id = 2, Name = "Metamorph", Age = 50, Description = "Under a lot of pressure" },
-            new Animal { Id = 3, Name = "Igno", Age = 50, Description = "Shiny and glasslike" },
+            new Animal { Id = 1, Name = "Sifu", Age = 50, Description = "I am Sifu" },
+            new Animal { Id = 2, Name = "Pabu", Age = 8, Description = "I am cute" },
+            new Animal { Id = 3, Name = "Red", Age = 10, Description = "I am red" },
         };
     }
 }
